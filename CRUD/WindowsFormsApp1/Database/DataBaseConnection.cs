9c841bbc4eac8b427b1c2e37bd08a2e8ec6e44ac
Batin Simsek <D201115@edu.rocwb.nl>
2022-04-05 18:06:46 +0200

    Database Connectie aangemaakt

---
+++ CRUD/WindowsFormsApp1/Database/DataBaseConnection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class DataBaseConnection
+    {
+        //database connectie met MICROSOFT SQL SERVER.
+        //LET OP DIE VAN JOU KAN ANDERS ZIJN!
+        public SqlConnection GetConnection()
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection("Data Source=.;Initial Catalog=CRUD;Integrated Security=True");
+                return connection;
+            }
+            catch (SqlException e) {
+                MessageBox.Show("Error " + e.Message);
+                return null;
+            }
+        }
+    }
+}
