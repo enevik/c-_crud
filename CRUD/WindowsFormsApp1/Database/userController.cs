8a5419fa0a60657e3c242628c9dc3efc5e5cad0c
Batin Simsek <D201115@edu.rocwb.nl>
2022-04-06 18:44:40 +0200

    SELECT FROM AANGEMAAKT

--- CRUD/WindowsFormsApp1/Database/userController.cs
+++ CRUD/WindowsFormsApp1/Database/userController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using WindowsFormsApp1.Domain;
 
 namespace WindowsFormsApp1.Database
@@ -14,9 +16,70 @@ namespace WindowsFormsApp1.Database
 
         //TODO show list aanmaken 
 
-        public ObservableCollection<User> getUserList()
+        //laat alle lijsten zien in userlist.
+        public List<User> getUserList()
         {
+            var userList = new List<User>();
+            //maakt een connectie database
+            SqlConnection con = db.GetConnection();
+            //open de con van de databased
+            con.Open();
+            //SQLcommend is nodig om een query uittevoeren
+            SqlCommand query = new SqlCommand("SELECT * FROM User");
+            
+            //try en catch spreken voorzicht
+            try
+            {
 
+                SqlDataAdapter da = new SqlDataAdapter(query);
+                //Leest de data
+                SqlDataReader reader = query.ExecuteReader();
+
+
+                while (reader.Read())
+                {
+                    //Haalt de gegevens op van Class User 0 is name enzovoort 
+                    User users = new User(
+                        reader.GetString(0),
+                        reader.GetString(1),
+                        reader.GetString(2),
+                        reader.GetString(3));
+                    //toevoegen in een list
+                    userList.Add(users);
+                }
+
+            }
+            catch (SqlException e)
+            {
+                // laat de error zien
+                MessageBox.Show("Error " + e.Message);
+                //sluit de verbinding met database
+                con.Close();
+                //daarna stuur die null
+                return null;
+            }
+
+            return userList;
+        }
+
+
+        public void insert()
+        {
+            SqlConnection con = db.GetConnection();
+            con.Open();
+
+            //TODO TOEVOEG EEN INSERT MODEL.
+            try
+            {
+
+                SqlCommand insert = new SqlCommand();
+                insert.ExecuteNonQuery();
+
+            }catch(SqlException e)
+            {
+                MessageBox.Show("Error " + e.Message);
+                con.Close();
+            }
         }
     }
 }
--- CRUD/WindowsFormsApp1/Domain/User.cs
+++ CRUD/WindowsFormsApp1/Domain/User.cs
@@ -16,17 +16,17 @@ namespace WindowsFormsApp1.Domain
 
         //Constructor aangemaakt
         public User(String Name, String Email, String PostalCode, String City) {
-            this.Name1 = Name;
-            this.Email1 = Email;
-            this.PostalCode1 = PostalCode;
-            this.City1 = City;
+            this.Name = Name;
+            this.Email = Email;
+            this.PostalCode = PostalCode;
+            this.City = City;
         }
 
 
-        //GETTER EN SETTER GEMAAKT!
-        public string Name1 { get => Name; set => Name = value; }
+        //GETTER EN SETTER GEMAAKT! MODEL!
+        public string Name1{ get => Name; set => Name = value; }
         public string Email1 { get => Email; set => Email = value; }
-        public string PostalCode1 { get => PostalCode; set => PostalCode = value; }
+        public string PostalCode1{ get => PostalCode; set => PostalCode = value; }
         public string City1 { get => City; set => City = value; }
     }
 }
