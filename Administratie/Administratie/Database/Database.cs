b09790a4a7940b67e1c9c424ef24fe73c95bced1
Batin Simsek <D201115@edu.rocwb.nl>
2022-04-13 20:53:46 +0200

    Applicatie overzetten naar wpf

---
+++ Administratie/Administratie/Database/Database.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Administratie.Database
+{
+    public class Database
+    {
+        //database connectie met MICROSOFT SQL SERVER.
+        //LET OP DIE VAN JOU KAN ANDERS ZIJN!
+        public SqlConnection GetConnection()
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection("Data Source=.;Initial Catalog=CRUD;Integrated Security=True");
+                return connection;
+            }
+            catch (SqlException e)
+            {
+                MessageBox.Show("Error " + e.Message);
+                return null;
+            }
+        }
+    }
+}
